56d4161b57d2b6cf0440bbfa3ee56b862bfe76d1
Shawn Roller <shawn@rollersoft.io>
2023-08-30 16:47:57 -0400

    Initial Commit

---
+++ SongClipperPOC/SongClipperPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongClipperPOCApp.swift
+//  SongClipperPOC
+//
+//  Created by Shawn Roller on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SongClipperPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
